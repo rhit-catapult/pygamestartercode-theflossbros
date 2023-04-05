6092747be471e538bf23a4df972419140550ca84
David Fisher <fisherds@rose-hulman.edu>
2023-04-05 09:37:42 -0400

    todo edits
    Making a more detailed todo 22

--- 04-Raindrops/RainyDay.py
+++ 04-Raindrops/RainyDay.py
@@ -8,10 +8,10 @@ class Raindrop:
     def __init__(self, screen, x, y):
         """ Creates a Raindrop sprite that travels down at a random speed. """
         # TODO 8: Initialize this Raindrop, as follows:
-        # TODO    - Store the screen.
-        # TODO    - Set the initial position of the Raindrop to x and y.
-        # TODO    - Set the initial speed to a random integer between 5 and 15.
-        # TODO  Use instance variables:   screen  x  y  speed.
+        #     - Store the screen.
+        #     - Set the initial position of the Raindrop to x and y.
+        #     - Set the initial speed to a random integer between 5 and 15.
+        #   Use instance variables:   screen  x  y  speed.
         pass
 
     def move(self):
@@ -28,7 +28,7 @@ class Raindrop:
     def draw(self):
         """ Draws this sprite onto the screen. """
         # TODO 9: Draw a vertical line that is 5 pixels long, 2 pixels thick,
-        # TODO     from the current position of this Raindrop (use either a black or blue color).
+        #      from the current position of this Raindrop (use either a black or blue color).
         pass
 
 
@@ -36,22 +36,22 @@ class Hero:
     def __init__(self, screen, x, y, with_umbrella_filename, without_umbrella_filename):
         """ Creates a Hero sprite (Mike) that does not move. If hit by rain he'll put up his umbrella. """
         # TODO 16: Initialize this Hero, as follows:
-        # TODO    - Store the screen.
-        # TODO    - Set the initial position of this Hero to x and y.
-        # TODO    - Create an image of this Hero WITH    an umbrella to the given with_umbrella_filename.
-        # TODO    - Create an image of this Hero WITHOUT an umbrella to the given without_umbrella_filename.
-        # TODO    - Set the "last hit time" to 0.
-        # TODO  Use instance variables:
-        # TODO     screen  x  y  image_umbrella   image_no_umbrella  last_hit_time.
+        #     - Store the screen.
+        #     - Set the initial position of this Hero to x and y.
+        #     - Create an image of this Hero WITH    an umbrella to the given with_umbrella_filename.
+        #     - Create an image of this Hero WITHOUT an umbrella to the given without_umbrella_filename.
+        #     - Set the "last hit time" to 0.
+        #   Use instance variables:
+        #      screen  x  y  image_umbrella   image_no_umbrella  last_hit_time.
         pass
 
     def draw(self):
         """ Draws this sprite onto the screen. """
         # TODO 17: Draw (blit) this Hero, at this Hero's position, WITHOUT an umbrella:
         # TODO 21: Instead draw (blit) this Hero, at this Hero's position, as follows:
-        # TODO    If the current time is greater than this Hero's last_hit_time + 1,
-        # TODO      draw this Hero WITHOUT an umbrella,
-        # TODO      otherwise draw this Hero WITH an umbrella.
+        #     If the current time is greater than this Hero's last_hit_time + 1,
+        #       draw this Hero WITHOUT an umbrella,
+        #       otherwise draw this Hero WITH an umbrella.
         pass
 
     def hit_by(self, raindrop):
@@ -64,12 +64,12 @@ class Cloud:
     def __init__(self, screen, x, y, image_filename):
         """ Creates a Cloud sprite that will produce Raindrop objects.  The cloud will be moving around. """
         # TODO 24: Initialize this Cloud, as follows:
-        # TODO    - Store the screen.
-        # TODO    - Set the initial position of this Cloud to x and y.
-        # TODO    - Set the image of this Cloud to the given image filename.
-        # TODO    - Create a list for Raindrop objects as an empty list called raindrops.
-        # TODO  Use instance variables:
-        # TODO     screen  x  y  image   raindrops.
+        #     - Store the screen.
+        #     - Set the initial position of this Cloud to x and y.
+        #     - Set the image of this Cloud to the given image filename.
+        #     - Create a list for Raindrop objects as an empty list called raindrops.
+        #   Use instance variables:
+        #      screen  x  y  image   raindrops.
         pass
 
     def draw(self):
@@ -80,9 +80,9 @@ class Cloud:
     def rain(self):
         """ Adds a Raindrop to the array of raindrops so that it looks like the Cloud is raining. """
         # TODO 28: Append a new Raindrop to this Cloud's list of raindrops,
-        # TODO    where the new Raindrop starts at:
-        # TODO      - x is a random integer between this Cloud's x and this Cloud's x + 300.
-        # TODO      - y is this Cloud's y + 100.
+        #     where the new Raindrop starts at:
+        #       - x is a random integer between this Cloud's x and this Cloud's x + 300.
+        #       - y is this Cloud's y + 100.
         pass
 
 
@@ -100,30 +100,34 @@ def main():
         # TODO 4:   Make the pygame.QUIT event stop the game.
 
         # TODO 27: Inside the game loop (AFTER the events loop above), get the list of keys that are currently pressed.
-        # TODO    Arrange so that the Cloud moves:
-        # TODO      5 pixels (or 10 pixels) to the right if the Right Arrow key (pygame.K_RIGHT) is pressed.
-        # TODO      5 pixels (or 10 pixels) to the left  if the Left  Arrow key (pygame.K_LEFT)  is pressed.
-        # TODO      5 pixels (or 10 pixels) up           if the Up    Arrow key (pygame.K_UP)    is pressed.
-        # TODO      5 pixels (or 10 pixels) down         if the Down  Arrow key (pygame.K_DOWN)  is pressed.
+        #     Arrange so that the Cloud moves:
+        #       5 pixels (or 10 pixels) to the right if the Right Arrow key (pygame.K_RIGHT) is pressed.
+        #       5 pixels (or 10 pixels) to the left  if the Left  Arrow key (pygame.K_LEFT)  is pressed.
+        #       5 pixels (or 10 pixels) up           if the Up    Arrow key (pygame.K_UP)    is pressed.
+        #       5 pixels (or 10 pixels) down         if the Down  Arrow key (pygame.K_DOWN)  is pressed.
         # DISCUSS: If you want something to happen once per key press, put it in the events loop above
         #          If you want something to continually happen while holding the key, put it after the events loop.
 
         # TODO 5: Inside the game loop, draw the screen (fill with white)
 
+        # --- begin area of test_drop code that will be removed later
         # TODO 12: As a temporary test, move test_drop
         # TODO 14: As a temporary test, check if test_drop is off screen, if so reset the y position to 10
         # TODO 10: As a temporary test, draw test_drop
 
         # TODO 20: As a temporary test, check if test_drop is hitting Mike (or Alyssa), if so set their last_hit_time
-        # TODO 22: When you run this test, slow the rain down to a speed of 2 to see the result, then remove that code
+        # TODO 22: Remove the code that reset the y of the test_drop when off_screen()
+        #          Instead reset the test_drop y to 10 when mike is hit, additionally set the x to 750
+        #          Then add similar code to alyssa that sets her last_hit_time and moves the test_drop to 10 320
+        # --- end area of test_drop code that will be removed later
 
         # TODO 26: Draw the Cloud.
 
         # TODO 29: Remove the temporary testdrop code from this function and refactor it as follows:
         # TODO: Make the Cloud "rain", then:
         # TODO    For each Raindrop in the Cloud's list of raindrops:
-            # TODO      - move the Raindrop.
-            # TODO      - draw the Raindrop.
+            #       - move the Raindrop.
+            #       - draw the Raindrop.
             # TODO  30: if the Hero (Mike or Alyssa) is hit by a Raindrop, set the Hero's last_time_hit to the current time.
             # Optional  - if the Raindrop is off the screen or hitting a Hero, remove it from the Cloud's list of raindrops.
 
